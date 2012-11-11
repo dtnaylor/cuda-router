842068ddf82c43f6f4fc1bda4760bf2cbb515f4b
Matt Mukerjee <mukerjee@cs.cmu.edu>
2012-11-11 17:21:02 -0500

    updated packet-collector to use select

--- packet-collector/packet-collector.cc
+++ packet-collector/packet-collector.cc
@@ -11,7 +11,7 @@
 #define MAX_ENTRIES 1024
 
 // in miliseconds
-#define TIMEOUT 100
+#define TIMEOUT 5000
 
 typedef struct _packet {
   int size;
@@ -20,7 +20,6 @@ typedef struct _packet {
 
 typedef struct _timeout {
   pthread_mutex_t lock;
-  int sockfd;
   int timeout;
 } timeout;
 
@@ -52,41 +51,50 @@ void *timer(void *data) {
   timeout *t = (timeout *)data;
   pthread_mutex_lock(&t->lock);
   t->timeout = 1;
-  shutdown(t->sockfd, SHUT_RDWR);
   pthread_mutex_unlock(&t->lock);
 }
 
-int get_packets(packet* p) {
+int get_packets(int sockfd, packet* p) {
   timeout t;
   pthread_t thread;
-
-  t.sockfd = init_socket();
-  if(t.sockfd == -1) {
-	return -1;
-  }
 
   t.timeout = 0;
   pthread_mutex_init(&t.lock, NULL);
 
+  struct timeval tout;
+  tout.tv_sec = 0;
+  tout.tv_usec = 0;
+
+  fd_set rfds;
+  FD_ZERO(&rfds);
+  FD_SET(sockfd, &rfds);
+
   pthread_create(&thread, NULL, timer, &t);
  
   int i;
-  for(i = 0; i < MAX_ENTRIES; i++) {
-	p[i].size = recv(t.sockfd, &p[i].buf, BUF_SIZE, 0); // need non-blocking i/o??
-	pthread_mutex_lock(&t.lock);
-	if(t.timeout == 1) break;
-	pthread_mutex_unlock(&t.lock);
+  for(i = 0; i < MAX_ENTRIES;) {
+    if(select(sockfd+1, &rfds, NULL, NULL, &tout)) {
+      p[i].size = recv(sockfd, &p[i].buf, BUF_SIZE, 0); 
+      i++;
+    }
+    pthread_mutex_lock(&t.lock);
+    if(t.timeout == 1) break;
+    pthread_mutex_unlock(&t.lock);
   }
   pthread_cancel(thread);
-  close(t.sockfd);
   return i;
 }
 
 int main() {  
-  packet* p = (packet *)malloc(sizeof(packet)*MAX_ENTRIES);
+  int sockfd = init_socket();
+  if(sockfd == -1) {
+    return -1;
+  }
 
+  packet* p = (packet *)malloc(sizeof(packet)*MAX_ENTRIES);
+  
   while(1) {
-	int num_packets = get_packets(p);
-	printf("i = %d\n", num_packets);
+    int num_packets = get_packets(sockfd, p);
+    printf("i = %d\n", num_packets);
   }
 }
