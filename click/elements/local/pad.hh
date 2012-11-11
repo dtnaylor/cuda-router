468afe66f7de65613742ff44e8d87d0f54393180
Matt Mukerjee <mukerjee@cs.cmu.edu>
2012-11-11 16:45:27 -0500

    adding click

---
+++ click/elements/local/pad.hh
@@ -0,0 +1,29 @@
+// -*- c-basic-offset: 4 -*-
+#ifndef CLICK_PAD_HH
+#define CLICK_PAD_HH
+#include <click/element.hh>
+CLICK_DECLS
+
+/*
+=c
+
+Pad
+
+=s basicmod
+
+*/
+
+class Pad : public Element { public:
+
+    Pad();
+    ~Pad();
+
+    const char *class_name() const		{ return "Pad"; }
+    const char *port_count() const		{ return PORTS_1_1; }
+
+    Packet *simple_action(Packet *);
+
+};
+
+CLICK_ENDDECLS
+#endif
