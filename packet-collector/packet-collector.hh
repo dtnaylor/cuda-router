187134f0cae773f8e260dc07aebbf0bd6bd02fc1
David Naylor <dnaylor@cs.cmu.edu>
2012-11-11 20:31:46 -0500

    Made header for packet collector, initial code for firewall -- right now it just does mat mult :)

---
+++ packet-collector/packet-collector.hh
@@ -0,0 +1,26 @@
+#ifndef PACKET_COLLECTOR_H
+#define PACKET_COLLECTOR_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <pthread.h>
+
+#define PORT 9877
+#define BUF_SIZE 4096
+#define MAX_ENTRIES 1024
+
+// in miliseconds
+#define TIMEOUT 100
+
+typedef struct _packet {
+  int size;
+  char buf[BUF_SIZE];
+} packet;
+
+int get_packets(int sockfd, packet* p);
+
+#endif /* PACKET_COLLECTOR_H */
