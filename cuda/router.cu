a56d7e19030e5c6ecbce6f6e267f33e2000ccacd
David Naylor <dnaylor@cs.cmu.edu>
2012-11-14 18:32:30 -0500

    Fixed some weird tab spacings

--- cuda/router.cu
+++ cuda/router.cu
@@ -26,13 +26,13 @@ int run(int argc, char **argv, int block_size, int sockfd)
 	unsigned int buf_size = sizeof(packet)*get_batch_size();
 	unsigned int results_size = sizeof(int)*get_batch_size();
 
-    // Allocate host memory for two batches of up to batch_size packets
+	// Allocate host memory for two batches of up to batch_size packets
 	// We will alternate between filling and processing these two buffers
 	// (at any given time one of the buffers will either be being filled
 	// or being processed)
-    packet* h_p1 = (packet *)malloc(buf_size);
+	packet* h_p1 = (packet *)malloc(buf_size);
 	check_malloc(h_p1, "h_p1", __LINE__);
-    packet* h_p2 = (packet *)malloc(buf_size);
+	packet* h_p2 = (packet *)malloc(buf_size);
 	check_malloc(h_p2, "h_p2", __LINE__);
 
 	// Allocate host memory for 2 arrays of results
@@ -41,20 +41,20 @@ int run(int argc, char **argv, int block_size, int sockfd)
 	int *h_results2 = (int*)malloc(results_size);
 	check_malloc(h_results2, "h_results2", __LINE__);
 
-    // Allocate device memory for up to batch_size packets
+	// Allocate device memory for up to batch_size packets
 	// TODO: wait and allocate only the amount needed after we receive?
-    packet *d_p1;
-    check_error(cudaMalloc((void **) &d_p1, buf_size), "cudaMalloc d_p1", __LINE__);
-    packet *d_p2;
-    check_error(cudaMalloc((void **) &d_p2, buf_size), "cudaMalloc d_p2", __LINE__);
+	packet *d_p1;
+	check_error(cudaMalloc((void **) &d_p1, buf_size), "cudaMalloc d_p1", __LINE__);
+	packet *d_p2;
+	check_error(cudaMalloc((void **) &d_p2, buf_size), "cudaMalloc d_p2", __LINE__);
 	// Allocate device memory for results
 	int *d_results1;
-    check_error(cudaMalloc((void **) &d_results1, results_size), "cudaMalloc d_results1", __LINE__);
+	check_error(cudaMalloc((void **) &d_results1, results_size), "cudaMalloc d_results1", __LINE__);
 	int *d_results2;
-    check_error(cudaMalloc((void **) &d_results2, results_size), "cudaMalloc d_results2", __LINE__);
+	check_error(cudaMalloc((void **) &d_results2, results_size), "cudaMalloc d_results2", __LINE__);
 
 
-    // Setup execution parameters
+	// Setup execution parameters
 	int threads_in_block = block_size;
 	int blocks_in_grid = get_batch_size() / threads_in_block;  // FIXME: optimize if we don't have a full batch
 	if (get_batch_size() % threads_in_block != 0) {
@@ -111,22 +111,22 @@ int run(int argc, char **argv, int block_size, int sockfd)
 	while(do_run) {
 		
 		/*************************************************************
-		 *                1) EXECUTE THE CUDA KERNEL                 *
+		 *				1) EXECUTE THE CUDA KERNEL				 *
 		 *************************************************************/
 		if (data_ready) { // First execution of loop: data_ready = false
 
 #ifdef MEASURE_PROCESSING_MICROBENCHMARK
-    		// Record the start event
-    		check_error(cudaEventRecord(start, NULL), "Record start event", __LINE__);
+			// Record the start event
+			check_error(cudaEventRecord(start, NULL), "Record start event", __LINE__);
 #endif /* MEASURE_PROCESSING_MICROBENCHMARK */
 
-    		// Execute the kernel
+			// Execute the kernel
 			printf("vvvvv   Begin processing %d packets   vvvvv\n\n", num_packets);
-    		process_packets<<< blocks_in_grid, threads_in_block >>>(d_p_current, d_results_current, num_packets, block_size);
+			process_packets<<< blocks_in_grid, threads_in_block >>>(d_p_current, d_results_current, num_packets, block_size);
 
 #ifdef MEASURE_PROCESSING_MICROBENCHMARK
-    		// Record the stop event
-    		check_error(cudaEventRecord(stop, NULL), "Record stop event", __LINE__);
+			// Record the stop event
+			check_error(cudaEventRecord(stop, NULL), "Record stop event", __LINE__);
 #endif /*MEASURE_PROCESSING_MICROBENCHMARK*/
 
 
@@ -139,7 +139,7 @@ int run(int argc, char **argv, int block_size, int sockfd)
 
 		
 		/*************************************************************
-		 *            2) COPY BACK RESULTS FROM LAST BATCH           *
+		 *			2) COPY BACK RESULTS FROM LAST BATCH		   *
 		 *************************************************************/
 		if (results_ready) { // First and second executions of loop: results_ready = false
 
@@ -151,7 +151,7 @@ int run(int argc, char **argv, int block_size, int sockfd)
 			// the kernel call we made above.
 
 			// Copy the last set of results back from the GPU
-    		check_error(cudaMemcpy(h_results_previous, d_results_previous, results_size, cudaMemcpyDeviceToHost), "cudaMemcpy (h_results, d_results)", __LINE__);
+			check_error(cudaMemcpy(h_results_previous, d_results_previous, results_size, cudaMemcpyDeviceToHost), "cudaMemcpy (h_results, d_results)", __LINE__);
 
 #ifdef MEASURE_LATENCY
 			gettimeofday(&lat_stop, NULL);
@@ -173,7 +173,7 @@ int run(int argc, char **argv, int block_size, int sockfd)
 		
 		
 		/*************************************************************
-		 *                  3) COPY NEXT BATCH TO GPU                *
+		 *				  3) COPY NEXT BATCH TO GPU				*
 		 *************************************************************/
 		// Get next batch of packets and copy them to the GPU
 		// FIXME: We're forcing the results from the current execution to wait
@@ -190,24 +190,24 @@ int run(int argc, char **argv, int block_size, int sockfd)
 #ifdef MEASURE_LATENCY
 		gettimeofday(lat_start_newest_next, NULL);
 #endif /* MEASURE_LATENCY */
-    	check_error(cudaMemcpy(d_p_next, h_p_next, buf_size, cudaMemcpyHostToDevice), "cudaMemcpy (d_p_next, h_p_next)", __LINE__);
+		check_error(cudaMemcpy(d_p_next, h_p_next, buf_size, cudaMemcpyHostToDevice), "cudaMemcpy (d_p_next, h_p_next)", __LINE__);
 
 
 
 		if (data_ready) {
 
 #ifdef MEASURE_PROCESSING_MICROBENCHMARK
-    		// Wait for the stop event to complete (which waits for the kernel to finish)
-    		check_error(cudaEventSynchronize(stop), "Failed to synchronize stop event", __LINE__);
-    		
+			// Wait for the stop event to complete (which waits for the kernel to finish)
+			check_error(cudaEventSynchronize(stop), "Failed to synchronize stop event", __LINE__);
+			
 			float msecTotal = 0.0f;
-    		check_error(cudaEventElapsedTime(&msecTotal, start, stop), "Getting time elapsed b/w events", __LINE__);
+			check_error(cudaEventElapsedTime(&msecTotal, start, stop), "Getting time elapsed b/w events", __LINE__);
 
-    		// Compute and print the performance
-    		printf(
-    		    "Performance= Time= %.3f msec, WorkgroupSize= %u threads/block\n",
-    		    msecTotal,
-    		    threads_in_block);
+			// Compute and print the performance
+			printf(
+				"Performance= Time= %.3f msec, WorkgroupSize= %u threads/block\n",
+				msecTotal,
+				threads_in_block);
 #else
 			// Wait for kernel execution to complete
 			check_error(cudaDeviceSynchronize(), "cudaDeviceSynchronize", __LINE__);
@@ -248,17 +248,17 @@ int run(int argc, char **argv, int block_size, int sockfd)
 
 
 
-    // Clean up memory
-    free(h_p1);
+	// Clean up memory
+	free(h_p1);
 	free(h_p2);
-    free(h_results1);
+	free(h_results1);
 	free(h_results2);
-    cudaFree(d_p1);
+	cudaFree(d_p1);
 	cudaFree(d_p2);
-    cudaFree(d_results1);
+	cudaFree(d_results1);
 	cudaFree(d_results2);
 
-    cudaDeviceReset();
+	cudaDeviceReset();
 
 	return EXIT_SUCCESS;
 }
@@ -268,11 +268,11 @@ void test(int sockfd)
 	printf("Batch Size: %d\n", get_batch_size());
 	
 	// Initialize a buffer for storing up to batch_size packets
-    packet* p = (packet *)malloc(sizeof(packet)*get_batch_size());
-    
-    while(1) {
-      int num_packets = get_packets(sockfd, p);
-      printf("i = %d\n", num_packets);
+	packet* p = (packet *)malloc(sizeof(packet)*get_batch_size());
+	
+	while(1) {
+	  int num_packets = get_packets(sockfd, p);
+	  printf("i = %d\n", num_packets);
 
 	  if (num_packets > 0) {
 	  	struct ip *ip_hdr = (struct ip*)p->buf;
@@ -281,7 +281,7 @@ void test(int sockfd)
 		printf("Source: %s (%u)\n", inet_ntoa(ip_hdr->ip_src), ntohs(udp_hdr->uh_sport));
 		printf("Next proto: %u\n", ip_hdr->ip_p);
 	  }
-    }
+	}
 }
 
 
@@ -292,8 +292,8 @@ int run_sequential(int argc, char **argv, int sockfd)
 	unsigned int buf_size = sizeof(packet)*get_batch_size();
 	unsigned int results_size = sizeof(int)*get_batch_size();
 
-    // Allocate buffer for packets
-    packet* p = (packet *)malloc(buf_size);
+	// Allocate buffer for packets
+	packet* p = (packet *)malloc(buf_size);
 	check_malloc(p, "p", __LINE__);
 
 	// Allocate array for results
@@ -407,66 +407,66 @@ void sig_handler (int sig)
 int main(int argc, char **argv)
 {
 
-    if (checkCmdLineFlag(argc, (const char **)argv, "help") ||
-        checkCmdLineFlag(argc, (const char **)argv, "?"))
-    {
-        printf("Usage -device=n (n >= 0 for deviceID)\n");
-        printf("      -batch=n  (Sets the number of packets in a batch; n > 0)\n");
-        printf("      -wait=n   (Sets how long we wait (milliseconds) for a complete batch of packets; n > 0)\n");
-        printf("      -block=n  (Sets the number of threads in a block; n > 0)\n");
-		printf("      -sequential  (runs router in CPU-only mode w/ sequential code)\n");
+	if (checkCmdLineFlag(argc, (const char **)argv, "help") ||
+		checkCmdLineFlag(argc, (const char **)argv, "?"))
+	{
+		printf("Usage -device=n (n >= 0 for deviceID)\n");
+		printf("	  -batch=n  (Sets the number of packets in a batch; n > 0)\n");
+		printf("	  -wait=n   (Sets how long we wait (milliseconds) for a complete batch of packets; n > 0)\n");
+		printf("	  -block=n  (Sets the number of threads in a block; n > 0)\n");
+		printf("	  -sequential  (runs router in CPU-only mode w/ sequential code)\n");
 
-        exit(EXIT_SUCCESS);
-    }
-    
+		exit(EXIT_SUCCESS);
+	}
+	
 	if (checkCmdLineFlag(argc, (const char **)argv, "batch"))
-    {
-        int size = getCmdLineArgumentInt(argc, (const char **)argv, "batch");
-        set_batch_size(size);
-    }
+	{
+		int size = getCmdLineArgumentInt(argc, (const char **)argv, "batch");
+		set_batch_size(size);
+	}
 	
 	if (checkCmdLineFlag(argc, (const char **)argv, "wait"))
-    {
-        int wait = getCmdLineArgumentInt(argc, (const char **)argv, "wait");
-        set_batch_wait(wait);
-    }
+	{
+		int wait = getCmdLineArgumentInt(argc, (const char **)argv, "wait");
+		set_batch_wait(wait);
+	}
 	
 	int block_size = DEFAULT_BLOCK_SIZE;
 	if (checkCmdLineFlag(argc, (const char **)argv, "block"))
-    {
-        int n = getCmdLineArgumentInt(argc, (const char **)argv, "block");
+	{
+		int n = getCmdLineArgumentInt(argc, (const char **)argv, "block");
 		if (n > 0) {
-        	block_size = n;
+			block_size = n;
 		}
-    }
+	}
 
-    // By default, we use device 0, otherwise we override the device ID based on what is provided at the command line
-    int devID = 0;
+	// By default, we use device 0, otherwise we override the device ID based on what is provided at the command line
+	int devID = 0;
 
-    if (checkCmdLineFlag(argc, (const char **)argv, "device"))
-    {
-        devID = getCmdLineArgumentInt(argc, (const char **)argv, "device");
-        cudaSetDevice(devID);
-    }
+	if (checkCmdLineFlag(argc, (const char **)argv, "device"))
+	{
+		devID = getCmdLineArgumentInt(argc, (const char **)argv, "device");
+		cudaSetDevice(devID);
+	}
 
-    cudaDeviceProp deviceProp;
-    check_error(cudaGetDevice(&devID), "cudaGetDevice", __LINE__);
-    check_error(cudaGetDeviceProperties(&deviceProp, devID), "cudaGetDeviceProperties", __LINE__);
+	cudaDeviceProp deviceProp;
+	check_error(cudaGetDevice(&devID), "cudaGetDevice", __LINE__);
+	check_error(cudaGetDeviceProperties(&deviceProp, devID), "cudaGetDeviceProperties", __LINE__);
 
-    if (deviceProp.computeMode == cudaComputeModeProhibited)
-    {
-        fprintf(stderr, "Error: device is running in <Compute Mode Prohibited>, no threads can use ::cudaSetDevice().\n");
-        exit(EXIT_SUCCESS);
-    }
+	if (deviceProp.computeMode == cudaComputeModeProhibited)
+	{
+		fprintf(stderr, "Error: device is running in <Compute Mode Prohibited>, no threads can use ::cudaSetDevice().\n");
+		exit(EXIT_SUCCESS);
+	}
 
-    printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID, deviceProp.name, deviceProp.major, deviceProp.minor);
+	printf("GPU Device %d: \"%s\" with compute capability %d.%d\n\n", devID, deviceProp.name, deviceProp.major, deviceProp.minor);
 	
 	
 	// Set up the socket for receiving packets from click
-    int sockfd = init_socket();
-    if(sockfd == -1) {
-      return -1;
-    }
+	int sockfd = init_socket();
+	if(sockfd == -1) {
+	  return -1;
+	}
 
 	//test(sockfd);
 	
@@ -475,12 +475,12 @@ int main(int argc, char **argv)
 	signal (SIGINT, sig_handler);
 
 	// Start the router!
-    if (checkCmdLineFlag(argc, (const char **)argv, "sequential"))
+	if (checkCmdLineFlag(argc, (const char **)argv, "sequential"))
 	{
 		return run_sequential(argc, argv, sockfd);
 	}
 	else
 	{
-    	return run(argc, argv, block_size, sockfd);
+		return run(argc, argv, block_size, sockfd);
 	}
 }
--- packet-collector/packet-collector.cc
+++ packet-collector/packet-collector.cc
@@ -4,25 +4,25 @@ int batch_size = DEFAULT_BATCH_SIZE;
 int batch_wait = DEFAULT_BATCH_WAIT;
 
 int init_socket() {
-  int sockfd;
-  struct sockaddr_in servaddr;
+	int sockfd;
+	struct sockaddr_in servaddr;
 
-  if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
+	if ((sockfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) < 0) {
 	perror("socket");
 	return -1;
-  }
+	}
 
-  memset(&servaddr, 0, sizeof(servaddr));
-  servaddr.sin_family      = AF_INET;
-  servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
-  servaddr.sin_port        = htons(PORT);
+	memset(&servaddr, 0, sizeof(servaddr));
+	servaddr.sin_family      = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+	servaddr.sin_port        = htons(PORT);
 
-  if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
+	if (bind(sockfd, (struct sockaddr *) &servaddr, sizeof(servaddr)) < 0) {
 	perror("bind");
 	return -1;
-  }
+	}
 
-  return sockfd;
+	return sockfd;
 }
 
 int set_batch_size(int s) {
@@ -48,49 +48,49 @@ int get_batch_wait() {
 }
 
 void *timer(void *data) {
-  usleep(batch_wait*1000);  
-  *(int *)data = 1;
-  return 0;
+	usleep(batch_wait*1000);	
+	*(int *)data = 1;
+	return 0;
 }
 
 int get_packets(int sockfd, packet* p) {
-  int timeout = 0;
-  pthread_t thread;
+	int timeout = 0;
+	pthread_t thread;
 
-  struct timeval tout;
-  tout.tv_sec = 0;
-  tout.tv_usec = 0;
+	struct timeval tout;
+	tout.tv_sec = 0;
+	tout.tv_usec = 0;
 
-  fd_set rfds;
-  FD_ZERO(&rfds);
+	fd_set rfds;
+	FD_ZERO(&rfds);
 
-  pthread_create(&thread, NULL, timer, &timeout);
+	pthread_create(&thread, NULL, timer, &timeout);
  
-  int i;
-  for(i = 0; i < batch_size;) {
-    FD_SET(sockfd, &rfds);
-    if(select(sockfd+1, &rfds, NULL, NULL, &tout) > 0) {
-      p[i].size = recv(sockfd, &p[i].buf, BUF_SIZE, 0); 
-      i++;
-    }
-    if(timeout == 1) break;
-  }
-  pthread_cancel(thread);
-  return i;
+	int i;
+	for(i = 0; i < batch_size;) {
+		FD_SET(sockfd, &rfds);
+		if(select(sockfd+1, &rfds, NULL, NULL, &tout) > 0) {
+			p[i].size = recv(sockfd, &p[i].buf, BUF_SIZE, 0); 
+			i++;
+		}
+		if(timeout == 1) break;
+	}
+	pthread_cancel(thread);
+	return i;
 }
 
 #ifndef CUDA_CODE
-int main() {  
-  int sockfd = init_socket();
-  if(sockfd == -1) {
-    return -1;
-  }
+int main() {	
+	int sockfd = init_socket();
+	if(sockfd == -1) {
+		return -1;
+	}
 
-  packet* p = (packet *)malloc(sizeof(packet)*batch_size);
-  
-  while(1) {
-    int num_packets = get_packets(sockfd, p);
-    printf("i = %d\n", num_packets);
-  }
+	packet* p = (packet *)malloc(sizeof(packet)*batch_size);
+	
+	while(1) {
+		int num_packets = get_packets(sockfd, p);
+		printf("i = %d\n", num_packets);
+	}
 }
 #endif
